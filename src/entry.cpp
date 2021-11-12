3ff44cecc64fe6ac97d2e8e0378dda6b4d1db13b
vopl <vopl@bk.ru>
2021-11-12 20:52:38 +0300

    .

---
+++ src/entry.cpp
@@ -0,0 +1,42 @@
+/* This file is part of the the dci project. Copyright (C) 2013-2021 vopl, shtoba.
+   This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public
+   License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+   This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+   You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>. */
+
+#include <dci/host/module/entry.hpp>
+#include <dci/logger.hpp>
+#include "example.hpp"
+
+namespace dci::module::example
+{
+    namespace
+    {
+        struct Manifest
+            : public dci::host::module::Manifest
+        {
+            Manifest()
+            {
+                _valid = true;
+                _name = dciModuleName;
+                _mainBinary = dciUnitTargetFile;
+            }
+        } manifest_;
+
+
+        struct Entry
+            : public dci::host::module::Entry
+        {
+            const Manifest& manifest() override
+            {
+                return manifest_;
+            }
+        } entry_;
+    }
+}
+
+extern "C"
+{
+    DCI_INTEGRATION_APIDECL_EXPORT dci::host::module::Entry* dciModuleEntry = &dci::module::example::entry_;
+}
